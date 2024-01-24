c160fdfeeb4678e6f365cbf0415b4c002040fd54
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-24 22:15:01 +0545

    feat: Added css layout for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import UserDetails from "./components/UserDetails";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <UserDetails />
     </>
   );
 };
---
+++ src/components/DeleteAllUser.jsx
@@ -0,0 +1,5 @@
+const DeleteAllUser = () => {
+  return <div>Delete All User</div>;
+};
+
+export default DeleteAllUser;
